6ee7a766fc3611948d19ae707b311162f1e671ff
Lumanze Nkemjika <thebogpharmacy@gmail.com>
2024-10-03 23:38:47 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php $txt = "i am working with profectionals";
+echo "$txt";
+$txt = ". i am becoming like them";
+echo "$txt"; 
+$txt = ". i have a son called phenihas" ;
+echo  "$txt";
+$txt = ". i stays in lagos state";
+echo "$txt ";
+$txt = ". i am a software enginee";
+echo "$txt";
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
